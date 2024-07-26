2a819f2fb2147ac61341d548af325e840b964b41
Akshith Mysa <akshithmysa@outlook.com>
2024-07-26 10:14:57 +0530

    File uploader done

---
+++ components/Documents.tsx
@@ -0,0 +1,12 @@
+import PlaceholderDocument from "./PlaceholderDocument"
+
+function Documents() {
+    return (
+        <div className="flex flex-wrap p-5 bg-gray-100 justify-center lg:justify-start rounded-sm gap-5 mx-auto max-w-7xl">
+            {/* Map though the documents */}
+
+            <PlaceholderDocument/>
+        </div>
+    )
+}
+export default Documents
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["i.imgur.com"],
+        dangerouslyAllowSVG: true
+    }
+};
 
 export default nextConfig;
